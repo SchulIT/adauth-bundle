e6cd8667d3f6806e76880c17ecf7ff1ac451b076
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-12-01 21:31:56 +0100

    Fixes Symfony 4.2 deprecation messages

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,8 +11,14 @@ class Configuration implements ConfigurationInterface {
      * @inheritDoc
      */
     public function getConfigTreeBuilder() {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('adauth');
+        $treeBuilder = new TreeBuilder('adauth');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('adauth');
+        }
 
         $rootNode
             ->children()
