dd1e6624f1323453bee693ffb5d77b4c3c1b5784
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-06-26 11:34:08 +0200

    Apply new configuration for failover scenarios

--- src/AdAuthBundle.php
+++ src/AdAuthBundle.php
@@ -18,26 +18,19 @@ class AdAuthBundle extends AbstractBundle {
     public function configure(DefinitionConfigurator $definition): void {
         $definition->rootNode()
             ->children()
-                ->scalarNode('url')->isRequired()->end()
-                ->arrayNode('tls')
-                    ->children()
-                        ->scalarNode('peer_name')->end()
-                        ->scalarNode('peer_fingerprint')->end()
-                        ->scalarNode('ca_certificate_file')->end()
-                    ->end()
-                ->end()
-                ->arrayNode('failover')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('url')->isRequired()->end()
-                        ->arrayNode('tls')
-                            ->children()
-                                ->scalarNode('peer_name')->end()
-                                ->scalarNode('peer_fingerprint')->end()
-                                ->scalarNode('ca_certificate_file')->end()
-                            ->end()
+                ->arrayNode('hosts')
+                    ->useAttributeAsKey('name')
+                    ->beforeNormalization()->castToArray()->end()
+                    ->requiresAtLeastOneElement()
+                    ->arrayPrototype()
+                        ->children()
+                            ->stringNode('url')->isRequired()->end()
+                            ->stringNode('peer_name')->end()
+                            ->stringNode('peer_fingerprint')->end()
+                            ->stringNode('ca_certificate_file')->end()
                         ->end()
                     ->end()
+                ->end()
             ->end();
     }
 
@@ -50,34 +43,24 @@ class AdAuthBundle extends AbstractBundle {
         $factory = new Definition(AdAuthFactory::class);
         $container->setDefinition('adauth.factory', $factory);
 
-        $primary = new Definition(AdAuth::class);
-        $primary->setFactory([$factory, 'createAdAuth']);
-        $primary->setArgument(0, $config['url']);
-        $primary->setArgument(1, $config['tls']);
-        $primary->setPublic(true);
+        $serviceReferences = [ ];
 
-        $container->setDefinition('adauth.primary', $primary);
+        foreach($config['hosts'] as $alias => $hostConfig) {
+            $service = new Definition(AdAuth::class);
+            $service->setFactory([$factory, 'createAdAuth']);
+            $service->setArgument(0, $hostConfig['url']);
+            $service->setArgument(1, $hostConfig);
 
-        if(isset($config['failover']['enabled']) && $config['failover']['enabled']) { // FAILOVER
-            $secondary = new Definition(AdAuth::class);
-            $secondary->setFactory([$factory, 'createAdAuth']);
-            $secondary->setArgument(0, $config['failover']['url']);
-            $secondary->setArgument(1, $config['failover']['tls']);
+            $id = sprintf('adauth.%s', $alias);
+            $serviceReferences[] = new Reference($id);
 
-            $container->setDefinition('adauth.secondary', $secondary);
-
-            $chain = new Definition(AdAuthInterface::class);
-            $chain->setClass(FailoverAdAuthChain::class);
-            $chain->setArgument(0, [
-                new Reference('adauth.primary'),
-                new Reference('adauth.secondary'),
-            ]);
-
-            $container->setDefinition('adauth', $chain);
-            $container->setAlias(AdAuthInterface::class, 'adauth');
-        } else { // NO FAILOVER
-            $container->setAlias(AdAuthInterface::class, 'adauth.primary');
-            $container->setAlias('adauth', 'adauth.primary');
+            $container->set($id, $service);
         }
+
+        $chain = new Definition(FailoverAdAuthChain::class);
+        $chain->setArgument(0, $serviceReferences);
+
+        $container->setDefinition('adauth', $chain);
+        $container->setAlias(AdAuthInterface::class, 'adauth');
     }
 }
